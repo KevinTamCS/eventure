a4ca57e1199e883d83ef0e1c7706c55bfba23b15
KevinTamCS <kevin@kevintamcs.com>
2020-08-22 19:59:44 -0400

    Add routes for new views

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,23 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './views/Home';
+import Join from './views/Join';
+import Login from './views/Login';
+import Welcome from './views/Welcome';
 
 const Routes = () => {
   return (
     <Switch>
-      <Route path="/">
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/join">
+        <Join />
+      </Route>
+      <Route exact path="/">
         <Home />
       </Route>
     </Switch>
