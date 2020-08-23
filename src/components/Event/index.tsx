7e8625715d236ffefc92fbab7b73669695d015a2
KevinTamCS <kevin@kevintamcs.com>
2020-08-23 18:59:28 -0400

    Migrate Event component to TypeScript

---
+++ src/components/Event/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import moment from 'moment-timezone';
+import {
+  EventBanner,
+  Card,
+  EventContainer,
+  EventInformation,
+  EventOrganizer,
+  EventStartTime,
+  EventTitle,
+} from './styles';
+
+interface Props {
+  banner?: string;
+  bannerAlt?: string;
+  title: string;
+  organizer: string;
+  startTime: Date;
+}
+
+const Event: React.FC<Props> = ({
+  banner,
+  bannerAlt,
+  title,
+  organizer,
+  startTime,
+}) => {
+  const startDateTime = moment(startTime).format('ddd, MMM D, YYYY, h:mm A z');
+
+  return (
+    <EventContainer>
+      <Card to="events/:id">
+        <EventBanner src={banner} alt={bannerAlt} />
+        <EventInformation>
+          <div>
+            <EventTitle>{title}</EventTitle>
+            <EventOrganizer>{organizer}</EventOrganizer>
+          </div>
+          <EventStartTime>{startDateTime}</EventStartTime>
+        </EventInformation>
+      </Card>
+    </EventContainer>
+  );
+};
+
+export default Event;
