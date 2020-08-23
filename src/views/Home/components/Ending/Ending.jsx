22f31cc8581e25ae7ae58eaccf6f80c747b293da
KevinTamCS <kevin@kevintamcs.com>
2020-08-22 20:53:20 -0400

    Fix Home ending section link

--- src/views/Home/components/Ending/Ending.jsx
+++ src/views/Home/components/Ending/Ending.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Button from 'components/Button';
 import { EndingContainer, Heading } from './styles';
+import { Link } from 'react-router-dom';
 
 const Ending = () => {
   return (
     <EndingContainer>
       <Heading>Start your journey for virtual events today.</Heading>
-      <Button center to="/join">
+      <Button as={Link} center to="/join">
         Join Eventure
       </Button>
     </EndingContainer>
