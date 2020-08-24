6e9f1a9bb58ae09fc9b030a44f06791b2a2dced1
KevinTamCS <kevin@kevintamcs.com>
2020-08-23 22:10:18 -0400

    Add Header styles

---
+++ src/components/Header/components/Logo/styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components/macro';
+
+export const NavbarLogo = styled.img`
+  height: 2rem;
+`;
---
+++ src/components/Header/components/Search/styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components/macro';
+
+export const NavbarSearch = styled.img`
+  margin-right: 1.5rem;
+`;
