1a457d801ed592a1d0807f49f92d0a1801bfcf6c
KevinTamCS <kevin@kevintamcs.com>
2020-08-25 23:02:20 -0400

    Remove excess Containers for Home page and return the page as a fragment

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 import Landing from './components/Landing';
 import FeaturedEvents from './components/FeaturedEvents';
 import EventCategory from './components/EventCategory';
 import Ending from './components/Ending';
-import { HomeContainer, TopContainer } from './styles';
 
 const Home = () => {
   const isLoggedIn: boolean = JSON.parse(
@@ -11,23 +11,19 @@ const Home = () => {
   );
 
   return (
-    <HomeContainer fluid className="p-0">
-      <TopContainer fluid="md">
-        {/* Display landing section for logged out users */}
+    <>
+      <Container fluid="md">
         {!isLoggedIn && <Landing />}
-
-        {/* Load the featured events for the user */}
         <FeaturedEvents isLoggedIn={isLoggedIn} />
-      </TopContainer>
+      </Container>
 
       {/* Slowly render as many event categories as the user has */}
       <EventCategory title="Photography" />
       <EventCategory title="Live Performances" />
       <EventCategory title="Health and Wellness" />
 
-      {/* Display ending section for logged out users */}
       {!isLoggedIn && <Ending />}
-    </HomeContainer>
+    </>
   );
 };
 
