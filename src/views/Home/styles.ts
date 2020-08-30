de9745817b70d7cb7e14f048848161fd1a10ea46
KevinTamCS <kevin@kevintamcs.com>
2020-08-30 16:27:45 -0400

    Adjust Home page top and bottom padding

--- src/views/Home/styles.ts
+++ src/views/Home/styles.ts
@@ -2,9 +2,9 @@ import styled from 'styled-components/macro';
 import Container from 'react-bootstrap/Container';
 
 export const StyledContainer = styled(Container)`
-  padding: 0 6.25vw;
+  padding: 0.5rem 6.25vw;
   
   @media screen and (min-width: 768px) {
-    padding: 0 3vw;
+    padding: 2rem 3vw;
   }
 `;
