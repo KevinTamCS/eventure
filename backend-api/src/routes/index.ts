0c70158f882ee2121f95c597adf052793129a964
KevinTamCS <kevin@kevintamcs.com>
2021-01-12 13:53:06 -0500

    Add root route for pinging

--- backend-api/src/routes/index.ts
+++ backend-api/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import events from '../routes/events';
 import users from '../routes/users';
 import auth from './auth';
+import root from './root';
 
 const router = Router();
 
 router.use('/events', events);
 router.use('/users', users);
 router.use('/', auth);
+router.use('/', root);
 
 export default router;
